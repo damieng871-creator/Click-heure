4f468ffc3c16bd43814d0d50348fd6282b414449
damieng871-creator <damieng871@gmail.com>
2025-08-12 22:53:15 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,49 +1,18 @@
-const CACHE_NAME = 'clickheure-v1';
-const CORE = [
-  './',
-  './index.html',
-  './manifest.webmanifest',
-  './icon-192.png',
-  './icon-512.png'
-];
-
-self.addEventListener('install', (e) => {
-  e.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(c => c.addAll(CORE))
-      .then(() => self.skipWaiting())
-  );
-});
-
-self.addEventListener('activate', (e) => {
-  e.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(keys.map(k => (k !== CACHE_NAME ? caches.delete(k) : null)))
-    ).then(() => self.clients.claim())
-  );
-});
-
-self.addEventListener('fetch', (e) => {
-  const url = new URL(e.request.url);
-
-  // Priorité au cache pour les fichiers de ton domaine (offline)
-  if (url.origin === location.origin) {
-    e.respondWith(
-      caches.match(e.request).then(res => res || fetch(e.request).then(r => {
-        const copy = r.clone();
-        caches.open(CACHE_NAME).then(c => c.put(e.request, copy));
-        return r;
-      }))
-    );
-    return;
-  }
-
-  // Pour les domaines externes : réseau d’abord, sinon cache
-  e.respondWith(
-    fetch(e.request).then(r => {
-      const copy = r.clone();
-      caches.open(CACHE_NAME).then(c => c.put(e.request, copy));
-      return r;
-    }).catch(() => caches.match(e.request))
-  );
-});
+<script>
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.register('./sw.js').then(reg => {
+    // si un SW est déjà prêt à remplacer l'ancien
+    if (reg.waiting) reg.waiting.postMessage({ type: 'SKIP_WAITING' });
+    // dès qu'une nouvelle version s'installe, on recharge
+    reg.addEventListener('updatefound', () => {
+      const nw = reg.installing;
+      nw && nw.addEventListener('statechange', () => {
+        if (nw.state === 'installed' && navigator.serviceWorker.controller) {
+          location.reload();
+        }
+      });
+    });
+  });
+  navigator.serviceWorker.addEventListener('controllerchange', () => location.reload());
+}
+</script>
